33f33f2a451cb5a5f0668ddb346887dcf5439a32
grmbyrn <graemebyrne_89@hotmail.com>
2023-04-10 13:32:57 +0200

    clean up actions and install and use axios

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,5 +1,13 @@
+import axios from 'axios'
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+const github = axios.create({
+    baseURL: GITHUB_URL,
+    headers: {
+        Authorization: `${GITHUB_TOKEN}`
+    }
+})
 
 // Get search results
 export const searchUsers = async (text) => {
@@ -8,46 +16,16 @@ export const searchUsers = async (text) => {
         q: text
     })
 
-    const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-        headers: {
-            Authorization: `${GITHUB_TOKEN}`
-        }
-    })
-    const {items} = await res.json()
-
-    return items
+    const response = await github.get(`/search/users?${params}`)
+    return response.data.items
 }
 
-    // Get a single user
-    export const getUser = async (login) => {
+// Get user and repos
+export const getUserAndRepos = async(login) => {
+    const [user, repos] = await Promise.all([
+        github.get(`/users/${login}`),
+        github.get(`/users/${login}/repos`)
+    ])
 
-        const res = await fetch(`${GITHUB_URL}/users/${login}`, {
-            headers: {
-                Authorization: `${GITHUB_TOKEN}`
-            }
-        })
-
-        if(res.status === 404){
-            window.location = '/notfound'
-        } else {
-            const data = await res.json()
-            return data
-        }
-    }
-
-    // Get user repos
-    export const getUserRepos = async (login) => {
-
-        const params = new URLSearchParams({
-            sort: 'created',
-            per_page: 10
-        })
-
-        const res = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-            headers: {
-                Authorization: `${GITHUB_TOKEN}`
-            }
-        })
-        const data = await res.json()
-        return data
-    }
+    return {user: user.data, repos: repos.data}
+}
